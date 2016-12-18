06c0212cfe10cf187772298b12969cf9a241e149
Faik TURAN <faikturan@users.noreply.github.com>
2016-12-18 14:37:39 +0300

    Add files via upload

--- ImageIO.java
+++ ImageIO.java
@@ -3,10 +3,15 @@ package com.faikturan;
 import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageFilter;
+import java.io.File;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileFilter;
 
 public class ImageIO extends JFrame {
 
@@ -27,20 +32,53 @@ public class ImageIO extends JFrame {
 	}
 	
 	public class PicturePanel extends JPanel{
-		public void paint(Graphics g){
+		public void paint(Graphics g)
+		{
 			g.drawImage(img, 0, 0, this);
 		}
 	}
 	
 	public void getPictureButtonClick(){
 		String file = getImageFile();
+		if (file != null) {
+			Toolkit kit = Toolkit.getDefaultToolkit();
+			img = kit.getImage(file);
+			img = img.getScaledInstance(500, -1, Image.SCALE_SMOOTH);
+			this.repaint();
+		}
 	}
 	
 	public String getImageFile(){
+		JFileChooser fc = new JFileChooser();
+		fc.setFileFilter(new ImageFilter());
 		return null;
 		
 		
 	}
+	
+	private class ImageFilter extends FileFilter{
+
+		
+		
+		@Override
+		public boolean accept(File f) {
+			if (f.isDirectory()) 
+				return true;
+			String name = f.getName();
+			if (name.matches(".*((.jpg) | (.gif) | (.png))"))
+				return true;
+			else
+			return false;
+		}
+
+		@Override
+		public String getDescription() {
+			return "Resim Dosyalarý (*.jpg, *.gif, *.png)";
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		new ImageIO();
 
